a0c2cfb8b2a84556d0c0723715c03f69c67b2031
MichaelRomero99 <39200121+MichaelRomero99@users.noreply.github.com>
2021-10-15 19:33:04 -0700

    Update farm.sql

--- database/farm.sql
+++ database/farm.sql
@@ -1,5 +1,13 @@
 USE OrganicFarm;
 
+CREATE TABLE IF NOT EXISTS user_credentials (
+  email VARCHAR (255) NOT NULL, 
+  salt VARCHAR (255) NOT NULL,
+  password VARCHAR (255) NOT NULL,
+  PRIMARY KEY (email)
+);
+
+
 CREATE TABLE IF NOT EXISTS farm_contact_info (
   id UUID,
   address VARCHAR(255) NOT NULL,
@@ -18,9 +26,3 @@ CREATE TABLE IF NOT EXISTS farm (
   FOREIGN KEY (contact_id)  REFERENCES farm_contact_info(id)
 );
 
-CREATE TABLE IF NOT EXISTS user_credentials (
-  email VARCHAR (255) NOT NULL, 
-  salt VARCHAR (255) NOT NULL,
-  password VARCHAR (255) NOT NULL,
-  PRIMARY KEY (email)
-);
