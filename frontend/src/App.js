5d2191f4de1d11a7d55ecb290f072166e4b166e3
Michael Romero <michaelromeroswe@gmail.com>
2021-10-21 17:02:08 -0700

    Removed fetchData() call from App.js since its not used

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,6 @@ export default function App() {
         { id: 4, title: "Golden Oaks Acre", inventory: [{ title: "Carrots", price: "$2/pound", img: "https://images.unsplash.com/photo-1447175008436-054170c2e979?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=999&q=80" }], img: "https://images.unsplash.com/photo-1585499193151-0f50d54c4e1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80" }
     ];
 
-    const fetchData = () => {
-        axios.get("http://localhost:8080/api/farmContactInfos").then(res => console.log(res))
-    }
-
     return (
         <BrowserRouter>
           <main>
@@ -28,8 +24,6 @@ export default function App() {
               </Route>
             </Switch>
           </main>
-
-          {fetchData()}
         </BrowserRouter>
     );
 }
