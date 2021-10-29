ebee473dc06332ac6a8b7511bef39fc37f49dc2f
Michael Romero <michaelromeroswe@gmail.com>
2021-10-28 19:21:59 -0700

    API's for Farm and Farm Contact Info

--- backend/src/main/java/com/hulk/organicfarm/models/Farm.java
+++ backend/src/main/java/com/hulk/organicfarm/models/Farm.java
@@ -1,25 +1,76 @@
 package com.hulk.organicfarm.models;
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Data
-//@Table(name = "farm")
-//public class Farm {
-//    @Id
-//    @Column(name = "id")
-//    private String id;
-//    @Column(name = "farm_name")
-//    private String farmName;
-//    @Column(name = "img_url")
-//    private String imgUrl;
-//    @Column(name = "contact_id")
-//    private String contactId;
-//}
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity(name= "Farm")
+@Table(
+        name= "farm"
+)
+
+public class Farm {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "uuid2")
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "farm_name", nullable = false)
+    private String farmName;
+
+    @Column(name = "img_url", nullable = false)
+    private String imgUrl;
+
+    @Column(name = "contact_id", nullable=false)
+    private String contactID;
+
+    @Column(name = "city")
+    private String city;
+
+    public Farm(String farmName, String imgUrl, String farmContactInfo, String city) {
+        this.farmName = farmName;
+        this.imgUrl = imgUrl;
+        this.contactID = farmContactInfo;
+        this.city = city;
+    }
+
+    public Farm(){ }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFarmName() {
+        return farmName;
+    }
+
+    public void setFarmName(String farmName) {
+        this.farmName = farmName;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getContactID() {
+        return contactID;
+    }
+
+    public void setContactID(String contactID) {
+        this.contactID = contactID;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
--- backend/src/main/java/com/hulk/organicfarm/repositories/FarmContactInfoRepository.java
+++ backend/src/main/java/com/hulk/organicfarm/repositories/FarmContactInfoRepository.java
@@ -1,12 +1,11 @@
 package com.hulk.organicfarm.repositories;
 
-//import com.hulk.organicfarm.models.FarmContactInfo;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.UUID;
+import com.hulk.organicfarm.models.FarmContactInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-//@Repository
-//public interface FarmContactInfoRepository extends JpaRepository<FarmContactInfo, UUID> {
-//
-//}
+import java.util.UUID;
+
+public interface FarmContactInfoRepository extends JpaRepository<FarmContactInfo, String> {
+
+}
+
